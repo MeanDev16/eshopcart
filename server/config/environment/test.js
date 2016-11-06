64d566f78f1f01d5db58ef492e4d11317334ad13
MeanDev16 <kondalrao_mca@yahoo.co.in>
2016-11-06 16:35:50 +0530

    second

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,6 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/angularfullstack-test'
+    uri: 'mongodb://dbuser:dbpassword@ds017514.mlab.com:17514/heroku_94r929qx' || 'mongodb://localhost/angularfullstack-test'
   }
 };
